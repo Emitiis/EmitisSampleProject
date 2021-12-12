84ea991c2e95d01e3c5429bfa498e832703ca388
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-12-11 21:15:54 -0900

    Click Action For Changing Color.

---
+++ app/src/main/java/co/geeksempire/emitis/sampleproject/KotlinOverview.kt
@@ -0,0 +1,23 @@
+package co.geeksempire.emitis.sampleproject
+
+class KotlinOverview {
+
+    var firstNumber: Int = 30
+    var secondNumber: Int = 10
+
+    // + - / *
+    var mathOperation: String = ""
+
+    var correctAnswer: Boolean = false
+
+    fun sumUpNumbers() {
+        println(firstNumber + secondNumber)
+
+    }
+
+    fun multiplyNumbers() {
+        println(firstNumber * secondNumber)
+
+    }
+
+}
