5ea02fcb8fd41db2fd87c4a656939184c5b2e0e4
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-12-26 22:59:42 -0900

    Initialize

--- app/src/main/java/co/geeksempire/emitis/sampleproject/Chat/OnlineChatActivity.kt
+++ app/src/main/java/co/geeksempire/emitis/sampleproject/Chat/OnlineChatActivity.kt
@@ -1,12 +1,24 @@
 package co.geeksempire.emitis.sampleproject.Chat
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.emitis.sampleproject.databinding.OnlineChatLayoutBinding
 
 class OnlineChatActivity : AppCompatActivity() {
 
+    lateinit var onlineChatLayoutBinding: OnlineChatLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onlineChatLayoutBinding = OnlineChatLayoutBinding.inflate(layoutInflater)
+        setContentView(onlineChatLayoutBinding.root)
+
+        val userNameText = intent.getStringExtra("USERNAME")
+
+        Toast.makeText(applicationContext, userNameText, Toast.LENGTH_LONG).show()
+
+        onlineChatLayoutBinding.userNameTextView.text = userNameText
     }
 
 }
--- app/src/main/java/co/geeksempire/emitis/sampleproject/ListOfContacts/Adapter/SimpleAdapter.kt
+++ app/src/main/java/co/geeksempire/emitis/sampleproject/ListOfContacts/Adapter/SimpleAdapter.kt
@@ -35,10 +35,12 @@ class SimpleAdapter (private val context: AppCompatActivity) : RecyclerView.Adap
 
         itemDataViewHolder.rootItemView.setOnClickListener {
 
-            Toast.makeText(context, inputSimpleListData[position].userName + " - " + inputSimpleListData[position].userAge,
-                Toast.LENGTH_LONG).show()
+//            Toast.makeText(context, inputSimpleListData[position].userName + " - " + inputSimpleListData[position].userAge,
+//                Toast.LENGTH_LONG).show()
 
-            val goToListIntent = Intent(context /*From Source Activity*/, OnlineChatActivity::class.java /*To Target Activity*/)
+            val goToListIntent = Intent(context /*From Source Activity*/, OnlineChatActivity::class.java /*To Target Activity*/).apply {
+                putExtra("USERNAME", inputSimpleListData[position].userName)
+            }
             goToListIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
 
             context.startActivity(goToListIntent)
