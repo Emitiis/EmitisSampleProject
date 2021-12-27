1c62649144ed8a055d1beca2e7608e81b2c15b10
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-12-26 23:01:59 -0900

    Initialize

--- app/src/main/java/co/geeksempire/emitis/sampleproject/Chat/OnlineChatActivity.kt
+++ app/src/main/java/co/geeksempire/emitis/sampleproject/Chat/OnlineChatActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import co.geeksempire.emitis.sampleproject.databinding.OnlineChatLayoutBinding
-import java.net.URI
-import java.net.URL
 
 class OnlineChatActivity : AppCompatActivity() {
 
